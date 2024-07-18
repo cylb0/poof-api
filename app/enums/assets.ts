5e164e10fda515ce3f183a560c236700dbd88e4e
Morgan <morgan.foucaut@greta-formations.fr>
2024-07-18 22:32:20 +0200

    feat: new SceneElementType enum

--- app/enums/assets.ts
+++ app/enums/assets.ts
@@ -3,3 +3,10 @@ export enum Assets {
   VIDEO = 'video',
   AUDIO = 'audio',
 }
+
+export enum SceneElementType {
+  IMAGE = Assets.IMAGE,
+  VIDEO = Assets.VIDEO,
+  AUDIO = Assets.AUDIO,
+  TEXT = 'text',
+}
