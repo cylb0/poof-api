26140ac3711e8c41638e75e20405f2b5c0d79efe
Morgan <morgan.foucaut@greta-formations.fr>
2024-07-18 20:49:11 +0200

    feat: Scene Validator now enforces exclusive use of IMAGE and VIDEO as Asset

--- app/validators/scene.ts
+++ app/validators/scene.ts
@@ -1,3 +1,6 @@
+import { Assets } from '#enums/assets'
+import PrivateAsset from '#models/private_asset'
+import PublicAsset from '#models/public_asset'
 import vine from '@vinejs/vine'
 
 /**
@@ -11,15 +14,26 @@ export const createSceneValidator = vine.compile(
     publicAssetId: vine
       .number()
       .exists(async (query, field) => {
-        const publicAsset = await query.from('public_assets').where('id', field).first()
-        return !!publicAsset
+        const publicAsset = (await query
+          .from('public_assets')
+          .where('id', field)
+          .first()) as PublicAsset
+        return (
+          !!publicAsset && (publicAsset.type === Assets.IMAGE || publicAsset.type === Assets.VIDEO)
+        )
       })
       .optional(),
     privateAssetId: vine
       .number()
       .exists(async (query, field) => {
-        const privateAsset = await query.from('private_assets').where('id', field).first()
-        return !!privateAsset
+        const privateAsset = (await query
+          .from('private_assets')
+          .where('id', field)
+          .first()) as PrivateAsset
+        return (
+          !!privateAsset &&
+          (privateAsset.type === Assets.IMAGE || privateAsset.type === Assets.VIDEO)
+        )
       })
       .optional(),
   })
