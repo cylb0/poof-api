14a46f5cbd064430812c9128b9db26f95ce6a030
Morgan <morgan.foucaut@greta-formations.fr>
2024-07-18 22:51:26 +0200

    feat: migration for scene_elements table

---
+++ database/migrations/1721329722774_create_scene_elements_table.ts
@@ -0,0 +1,33 @@
+import { SceneElementType } from '#enums/assets'
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'scene_elements'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('scene_id').unsigned().references('scenes.id').onDelete('CASCADE')
+      table.enum('type', Object.values(SceneElementType)).notNullable()
+      table.integer('z_index')
+      table.integer('public_asset_id').unsigned().references('public_assets.id').onDelete('CASCADE')
+      table
+        .integer('private_asset_id')
+        .unsigned()
+        .references('private_assets.id')
+        .onDelete('CASCADE')
+      table.string('text_content')
+      table.float('from')
+      table.float('duration')
+      table.json('position')
+      table.json('dimension')
+      table.json('metadata')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
