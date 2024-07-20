7944180fe788ce8b1232392b2b4825107a78e41a
Morgan <morgan.foucaut@greta-formations.fr>
2024-07-20 22:42:01 +0200

    test: fixed users_controller test

--- tests/functional/users_controller/index/users_controller.spec.ts
+++ tests/functional/users_controller/index/users_controller.spec.ts
@@ -1,9 +1,8 @@
 import { test } from '@japa/runner'
 
 test.group('GET /users', () => {
-  test('should return a 403 error when unauthorized', async ({ client }) => {
+  test('should return a 401 error when unauthorized', async ({ client }) => {
     const response = await client.get('/users')
-    response.assertStatus(403)
-    response.assertBody({ message: `You cannot access this resource.` })
+    response.assertStatus(401)
   })
 })
