a27e6c6037f459fc35ec29e38c9fb4bb22e1b0bb
Morgan <morgan.foucaut@greta-formations.fr>
2024-07-23 20:03:55 +0200

    feat: new constant api response messages

---
+++ app/constants/api_response_messages.ts
@@ -0,0 +1,25 @@
+export const FORBIDDEN_MESSAGE = 'You cannot access this resource.'
+/**
+ * Response message for index method of resources controllers
+ */
+export const RESOURCE_LIST_FOUND = 'Resources found.'
+
+/**
+ * Response message for show method of resources controllers
+ */
+export const RESOURCE_FOUND = 'Resource found.'
+
+/**
+ * Response message for resource creation success
+ */
+export const RESOURCE_CREATION_SUCCESS = 'Resource creation success.'
+
+/**
+ * Response message for resource update success
+ */
+export const RESOURCE_UPDATE_SUCCESS = 'Resource update success.'
+
+/**
+ * Response message for resource delete success
+ */
+export const RESOURCE_DELETE_SUCCESS = 'Resource delete success.'
--- app/constants/exception_messages.ts
+++
@@ -1,1 +0,0 @@
-export const FORBIDDEN_MESSAGE = 'You cannot access this resource.'
